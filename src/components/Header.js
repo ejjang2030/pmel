ed0d5ff1b92f3ec84c019f219386eabd8e241e1f
Ejjang2030 <ejjang2030@gmail.com>
2021-10-29 05:31:57 +0900

    문구 수정

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,14 +7,22 @@ const Header = () => {
     
     return (
         <div className="header-wraper">
+            <div className="container">
+                <div className="content">
+                    <div className="globe"></div>
+                    <h2 className="frame-1">PMEL</h2>
+                    <h2 className="frame-2">문화를 만듭니다.</h2>
+                    <h2 className="frame-3">We Make Culture.</h2>
+                </div>
+            </div>
             <div className="main-info">
-                <Typed
+                {/* <Typed
                     className="typed-text"
-                    strings={["PMEL", "MUSIC", "PRODUCING", "RECORDING"]}
+                    strings={["PMEL", "문화를 만듭니다.", "PRODUCING", "RECORDING"]} // 문화를 만듭니다.
                     typeSpeed={80}
                     backSpeed={100}
                     loop
-                />
+                /> */}
                 
             </div>
         </div>
